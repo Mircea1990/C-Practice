88ab0048cbd95816835731e6b89ae39dff6d92f9
Mircea1990 <41681031+Mircea1990@users.noreply.github.com>
2021-04-03 18:13:28 +0300

    Add files via upload

---
+++ Working with Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Working_with_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1
+            string s1 = "abc";
+            string s2 = s1;
+            Console.WriteLine(s1);
+            s1 = s1.Replace("abc", "xz");
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine();
+
+            //Ex2
+            string s3 = "kmn";
+            string s4 = s3;
+            s3 += "d";
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+        }
+    }
+}
